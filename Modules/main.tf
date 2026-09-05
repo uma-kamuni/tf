9afb144f174c6f4fe7824a8ad743bc30f7bd9228
uma-kamuni <kamuniumaramesh.gmail.com>
2026-09-05 16:29:10 +0530

    tf

--- Modules/main.tf
+++ Modules/main.tf
@@ -3,8 +3,8 @@ module "vpc" {
     vpc_cidr = "10.0.0.0/16"
     public_subnet_cidr = "10.0.1.0/24"
     private_subnet_cidr = "10.0.2.0/24"
-    public_az = "us-east-1a"
-    private_az = "us-east-1b"
+    public_az = "eu-north-1a"
+    private_az = "eu-north-1b"
     sg_name = "my_security_group"
     http_port = 80
     ssh_port = 22
--- Modules/output.tf
+++ Modules/output.tf
@@ -1,3 +1,3 @@
 output "public_ip" {
-  value = module.EC2.public_instance_ip
+  value = module.ec2.public_instance_ip
 }
