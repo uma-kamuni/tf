3c8458298dc12cee59237524ffae4421cfa6613c
uma-kamuni <kamuniumaramesh.gmail.com>
2026-09-05 16:40:21 +0530

    tf

--- Modules/module/EC2/user_data.sh
+++ Modules/module/EC2/user_data.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-dnf update -y
-dnf install nginx -y
+apt update -y
+apt install nginx -y
 systemctl enable nginx
 systemctl start nginx
 #remove index.html
 rm -f /var/www/html/index.html
 echo "<h1> HELLO UMAA !!!</h1>" > /var/www/html/index.html
+systemctl restart nginx
