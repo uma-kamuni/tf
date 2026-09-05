10b10ab3f49708c917462d2e812af4fcd808e152
uma-kamuni <kamuniumaramesh.gmail.com>
2026-09-05 16:11:30 +0530

    tf

--- Modules/modules/EC2/main.tf
+++ Modules/modules/EC2/main.tf
@@ -22,5 +22,5 @@ resource "aws_instance" "private_instance" {
         Name = "private_instance"
     
     }
-    user_data = file("/root/tf/Modules/EC2/user_data.sh")
+    user_data = file("/root/tf/Modules/modules/EC2/user_data.sh")
 }
